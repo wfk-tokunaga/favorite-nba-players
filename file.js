5056b79a6c53da533ba921bf1f954a5a624c09a3
wfk-tokunaga <wktokunaga@gmail.com>
2022-08-01 14:58:41 -0700

    added nba player pictures

--- file.js
+++ file.js
@@ -9,5 +9,28 @@ names.forEach((name, index) => {
 });
 
 const removedNoNames = nameLinks.filter(player => player[0] !== 'Default player headshot');
+removedNoNames.forEach(player => {
+    let url = player[1];
+    // let fileName = player[0].toLowerCase().split(' ');
+    let fileName = player[0].toLowerCase().split(' ').join('-').replaceAll('.', '').replaceAll('\'', '');
 
-console.log(removedNoNames);
+    console.log(fileName);
+    // console.log(fileName.join('-').replaceAll('.', '').replaceAll('\'', ''));
+
+    let dest = `./assets/images/${fileName}.png`
+        // wget({url: url, dest: destination_folder_or_filename}, callback);
+    wget({ url: url, dest: dest },
+        function(error, response, body) {
+            if (error) {
+                console.log('--- error:');
+                console.log(error); // error encountered
+            } else {
+                console.log('--- headers:');
+                console.log(response.headers); // response headers
+                console.log('--- body:');
+                console.log(body); // body properties { bodyUsed: true, size: 1059, timeout: 2000 }
+            }
+        });
+})
+
+// console.log(removedNoNames);
